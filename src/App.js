a9beb59e2d6cab205c7441cf21a363f26da9f864
udeshya17 <udeshyasharma17@gmail.com>
2024-07-08 20:20:44 +0530

    card part done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="container">
+        <p className='heading'>Expense Tracker</p>
+        <div className='content'>
+        <div className='cardOne'>
+          <p className='subHead'>Wallet Balance: <span style={{color: '#9DFF5B'}}>₹5000</span></p>
+          <button className='addButton' style={{background: 'linear-gradient(90deg, #B5DC52 0%, #89E148 100%)'}}>+ Add Income</button>
+        </div>
+        <div className='cardTwo'>
+          <p className='subHead'>Expenses: <span style={{color: '#9DFF5B'}}>₹500</span></p>
+          <button className='addButton' style={{background: 'linear-gradient(90deg, #FF9595 0%, #FF4747 80%, #FF3838 100%)'}}>+ Add Expense</button>
+        </div>
+        <div className='pieChart'>
+          
+        </div>
+        </div>
+      </div>
   );
 }
 
