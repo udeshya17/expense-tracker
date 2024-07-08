d6678e2e7fa93b2fe1ca3c9f832a28c167b13d6b
udeshya17 <udeshyasharma17@gmail.com>
2024-07-09 01:05:23 +0530

    Frontend done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import React from 'react'
+import React from 'react';
 import './App.css';
+import Chart from './components/PieChart/PieChart'
 
 function App() {
   return (
     <div className="container">
-        <p className='heading'>Expense Tracker</p>
-        <div className='content'>
+      <p className='heading'>Expense Tracker</p>
+      <div className='content'>
         <div className='cardOne'>
-          <p className='subHead'>Wallet Balance: <span style={{color: '#9DFF5B'}}>₹5000</span></p>
-          <button className='addButton' style={{background: 'linear-gradient(90deg, #B5DC52 0%, #89E148 100%)'}}>+ Add Income</button>
+          <p className='subHead'>Wallet Balance: <span style={{ color: '#9DFF5B' }}>₹5000</span></p>
+          <button className='addButton' style={{ background: 'linear-gradient(90deg, #B5DC52 0%, #89E148 100%)' }}>+ Add Income</button>
         </div>
         <div className='cardTwo'>
-          <p className='subHead'>Expenses: <span style={{color: '#9DFF5B'}}>₹500</span></p>
-          <button className='addButton' style={{background: 'linear-gradient(90deg, #FF9595 0%, #FF4747 80%, #FF3838 100%)'}}>+ Add Expense</button>
+          <p className='subHead'>Expenses: <span style={{ color: '#F4BB4A'}}>₹500</span></p>
+          <button className='addButton' style={{ background: 'linear-gradient(90deg, #FF9595 0%, #FF4747 80%, #FF3838 100%)' }}>+ Add Expense</button>
         </div>
-        <div className='pieChart'>
-          
-        </div>
-        </div>
+        <Chart />
       </div>
+      <div className='subContents'>
+        <div>
+          <p className='subContentHead'>Recent transactions</p>
+            <div className='subContainerOne'>
+
+            </div>
+        </div>
+        <div>
+          <p className='subContentHead'>Top Expenses</p>
+            <div className='subContainerTwo'>
+
+            </div>
+        </div>
+        
+      </div>
+    </div>
   );
 }
 
---
+++ src/components/PieChart/PieChart.jsx
@@ -0,0 +1,65 @@
+import React from 'react';
+import { PieChart, Pie, Cell, Tooltip, Legend } from 'recharts';
+import styles from './PieChart.module.css';
+
+const data = [
+    { name: 'Food', value: 700 },
+    { name: 'Entertainment', value: 300 },
+    { name: 'Travel', value: 200 },
+  ];
+  
+  const COLORS = [ '#FF9304' , '#A000FF', '#FDE006'];
+  
+  const RADIAN = Math.PI / 180;
+  const renderCustomizedLabel = ({
+    cx, cy, midAngle, innerRadius, outerRadius, percent, index
+  }) => {
+    const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+    const x = cx + radius * Math.cos(-midAngle * RADIAN);
+    const y = cy + radius * Math.sin(-midAngle * RADIAN);
+  
+    return (
+      <text
+        x={x}
+        y={y}
+        fill="white"
+        textAnchor={x > cx ? 'start' : 'end'}
+        dominantBaseline="central"
+      >
+        {`${(percent * 100).toFixed(0)}%`}
+      </text>
+    );
+  };
+
+  function Chart(){
+    return (
+        <div className={styles.pieChart}>
+          <PieChart width={200} height={200}>
+            <Pie
+              data={data}
+              cx={100}
+              cy={100}
+              labelLine={false}
+              label={renderCustomizedLabel}
+              outerRadius={95}
+              fill="#8884d8"
+              dataKey="value"
+            >
+              {data.map((entry, index) => (
+                <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+              ))}
+            </Pie>
+          </PieChart>
+          <div className={styles.legend}>
+            {data.map((entry, index) => (
+              <div key={`legend-${index}`} className={styles.legendItem}>
+                <span className={styles.legendColor} style={{ backgroundColor: COLORS[index % COLORS.length] }}></span>
+                <span className={styles.legendText}>{entry.name}</span>
+              </div>
+            ))}
+          </div>
+        </div>
+    )
+  }
+
+  export default Chart;
